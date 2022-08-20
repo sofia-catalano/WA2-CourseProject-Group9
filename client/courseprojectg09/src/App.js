31c7c11d110f8352f5cca887be3811d44d090596
sofia-catalano <79850786+sofia-catalano@users.noreply.github.com>
2022-08-20 16:56:55 +0200

    Login form

--- client/courseprojectg09/src/App.js
+++ client/courseprojectg09/src/App.js
@@ -6,11 +6,12 @@ import {useState, useEffect} from 'react';
 import Sidebar from "./components/Sidebar/Sidebar";
 import Container from "@mui/material/Container";
 import RegistrationPage from "./components/Registration/RegistrationPage";
+import LoginPage from "./components/Login/LoginPage";
 
 function App() {
 
-      const [userRole, setUserRole] = useState('admin');
-      const navigate = useNavigate();
+    const [userRole, setUserRole] = useState('admin');
+    const navigate = useNavigate();
     const [toggled, setToggled] = useState(true);
     const [collapsed, setCollapsed] = useState(false);
 
@@ -34,6 +35,7 @@ function App() {
           <Sidebar {...AsideProps} />
               <Routes>
                   <Route exact path="/" element={<Homepage/>}/>
+                  <Route exact path="/user/login" element={<LoginPage/>}/>
                   <Route exact path="/user/register" element={<RegistrationPage/>}/>
               </Routes>
       </>
---
+++ client/courseprojectg09/src/components/Login/LoginPage.js
@@ -0,0 +1,71 @@
+import './LoginPage.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+export default function LoginPage() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box id="loginForm">
+                    <Avatar id="loginIcon" sx={{ width: 60, height: 60, mb: 1, bgcolor: '#1976d2' }}>
+                        <LockOutlinedIcon fontSize="large" sx={{color: '#ffeb3b' }}/>
+                    </Avatar>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{p: 2}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Login
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
