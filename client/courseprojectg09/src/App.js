7fc461ec5149be40c61640b59114e84762033d76
Mariagrazia <mariagrazia.paladino1@gmail.com>
2022-08-20 12:55:46 +0200

    Added sidebar

--- client/courseprojectg09/src/App.js
+++ client/courseprojectg09/src/App.js
@@ -2,15 +2,38 @@ import { Routes, BrowserRouter, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./components/Homepage/Homepage";
+import {useState, useEffect} from 'react';
+import Sidebar from "./components/Sidebar/Sidebar";
+import Container from "@mui/material/Container";
 
 function App() {
-  const navigate = useNavigate();
+
+      const [userRole, setUserRole] = useState('admin');
+      const navigate = useNavigate();
+    const [toggled, setToggled] = useState(true);
+    const [collapsed, setCollapsed] = useState(false);
+
+    const handleCollapse = (value) => {
+        setCollapsed(value);
+    };
+
+    const handleToggle = () => {
+        setToggled(!toggled);
+    };
+    const AsideProps = {
+        toggled,
+        collapsed,
+        userRole,
+        handleToggle,
+        handleCollapse,
+    }
   return (
-      <>
-              <Navbar/>
-              <Routes>
-                  <Route exact path="/" element={<Homepage/>}/>
-              </Routes>
+     <>
+          <Navbar/>
+          <Sidebar {...AsideProps} />
+          {/*<Routes>
+                 <Route exact path="/" element={<Homepage/>}/>
+          </Routes>*/}
       </>
   );
 }
