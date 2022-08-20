22fede5ef408eb7fc1c8f7de010ac176682d3a40
francesco-plc <francescopolicastro1997@gmail.com>
2022-08-20 11:53:29 +0200

    set up sign up component

--- client/courseprojectg09/src/App.js
+++ client/courseprojectg09/src/App.js
@@ -2,6 +2,7 @@ import { Routes, BrowserRouter, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./components/Homepage/Homepage";
+import RegistrationPage from "./components/Registration/RegistrationPage";
 
 function App() {
   const navigate = useNavigate();
@@ -10,6 +11,7 @@ function App() {
               <Navbar/>
               <Routes>
                   <Route exact path="/" element={<Homepage/>}/>
+                  <Route exact path="/user/register" element={<RegistrationPage/>}/>
               </Routes>
       </>
   );
