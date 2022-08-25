e1b56d5717c875cadcdacf56b81c1cc522978c42
sofia-catalano <79850786+sofia-catalano@users.noreply.github.com>
2022-08-25 16:17:13 +0200

    Update Users list

--- client/courseprojectg09/src/components/admin/UsersList.js
+++ client/courseprojectg09/src/components/admin/UsersList.js
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
 import { Link as RouterLink} from 'react-router-dom';
 
 function UsersList(props) {
@@ -12,8 +13,8 @@ function UsersList(props) {
     const data = ["user1", "user2", "user3", "user4"]
     const style = {
         width: '100%',
-        maxWidth: 700,
-        bgcolor: 'background.paper',
+        maxWidth: 850,
+        bgcolor: 'bckground.paper',
         marginTop: 5,
         marginLeft: 10
     };
@@ -23,9 +24,9 @@ function UsersList(props) {
             ?
             <CircularProgress />
             :
-            <Box>
+            <Box sx={{width: '90%', mt: 2, mr: 5, ml: 5}}>
                 <Typography
-                    sx={{ color:'#1976d2'}}
+                    sx={{color: '#1976d2'}}
                     variant="h4"
                     id="title"
                     component="div"
@@ -33,34 +34,38 @@ function UsersList(props) {
                 >
                     Users list
                 </Typography>
-                <List sx={style} component="nav" aria-label="user-list">
-                    {data.map( user => {
-                        return(
-                            <Box key={user}>
-                                <ListItem>
-                                    <Stack spacing={4} direction="row">
-                                        <Typography sx={{mr: 5}} variant="subtitle1" gutterBottom>
-                                            {user}
-                                        </Typography>
-                                        <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/profile`}}>
-                                            <Button variant="outlined">Profile</Button>
-                                        </Link>
-                                        <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/orders`}}>
-                                            <Button variant="outlined">Orders</Button>
-                                        </Link>
-                                        <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/tickets`}}>
-                                            <Button variant="outlined">Tickets</Button>
-                                        </Link>
-                                        <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/travelcards`}}>
-                                            <Button variant="outlined">Travelcards</Button>
-                                        </Link>
-                                    </Stack>
-                                </ListItem>
-                                <Divider/>
-                            </Box>
+                <Box sx={{width: '90%', mt: 2, mr: 5, ml: 5}}>
+                    <Paper sx={{width: '100%'}}>
+                        <List sx={style} aria-label="user-list">
+                        {data.map( user => {
+                            return(
+                                <Box textAlign="center">
+                                    <ListItem key={user}>
+                                        <Stack spacing={10} direction="row">
+                                            <Typography sx={{mr: 5}} variant="subtitle1" gutterBottom>
+                                                {user}
+                                            </Typography>
+                                            <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/profile`}}>
+                                                <Button variant="outlined">Profile</Button>
+                                            </Link>
+                                            <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/orders`}}>
+                                                <Button variant="outlined">Orders</Button>
+                                            </Link>
+                                            <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/tickets`}}>
+                                                <Button variant="outlined">Tickets</Button>
+                                            </Link>
+                                            <Link component={RouterLink} to={{pathname: `/admin/traveler/${user}/travelcards`}}>
+                                                <Button variant="outlined">Travelcards</Button>
+                                            </Link>
+                                        </Stack>
+                                    </ListItem>
+                                    <Divider/>
+                                </Box>
+                            )}
                         )}
-                    )}
-                </List>
+                        </List>
+                    </Paper>
+                </Box>
             </Box>
         }
         </>
