89ce0a7f5622f882f160ff89f8c9d4a0d150191e
francesco-plc <francescopolicastro1997@gmail.com>
2022-09-08 14:07:49 +0200

    homepage overflow fixed (a chiacchiere)

--- client/courseprojectg09/src/components/Homepage/Homepage.js
+++ client/courseprojectg09/src/components/Homepage/Homepage.js
@@ -4,15 +4,13 @@ import OutlinedCard from "./HomepageCard";
 
 function Homepage() {
     return (
-        <div style={{ overflow: "hidden"}}>
-            <div style={{ overflowX: "hidden", position: "relative", maxHeight: "93vh", overflowY: "hidden", justifyContent: "center" }}>
-                <img style={{ maxHeight: "92vh", width: "100vw", marginLeft: "0rem", marginRight: "0rem" }} src={wallpaper} alt={"homepage"}>
-                </img>
-                <div className="overlayCard">
-                    <OutlinedCard/>
-                </div>
+        <div style={{overflow: "hidden", position: "relative", maxHeight: "90.3vh", justifyContent: "center"}}>
+            <img style={{maxHeight: "91vh", width: "100vw", marginLeft: "0rem", marginRight: "0rem"}} src={wallpaper}
+                 alt={"homepage"}>
+            </img>
+            <div className="overlayCard">
+                <OutlinedCard/>
             </div>
-
         </div>
     );
 }
